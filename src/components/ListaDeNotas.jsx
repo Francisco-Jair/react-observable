dc240c5e93587efd46005039c1dcdf8e43b2c68b
Francisco Jair <fjair123@ufpi.edu.br>
2021-08-07 15:02:54 -0300

    App for create note in react

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import CardNotas from "./CardNotas";
+import "../css/ListaDeNotas.css";
+
+class ListaDeNotas extends Component {
+  constructor(props) {
+    super(props);
+    // Esse construtor não é preciso, ele recebe as propriedade sem o construtor
+  }
+
+  render() {
+    return (
+      <ul className="lista-notas">
+        {/* Usando javascript com JSX */}
+        {this.props.notas.map((nota, index) => {
+          return (
+            <li className="lista-notas_item" key={index}>
+              <CardNotas titulo={nota.titulo} texto={nota.texto} />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
